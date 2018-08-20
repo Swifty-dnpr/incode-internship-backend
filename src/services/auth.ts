220138955246be4fc47d419c5ea6d444779a3dbf
Max <shevchenkomaksym93@gmail.com>
2018-08-20 16:27:28 +0300

    additional auth improvements

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,20 +1,24 @@
-import { DatabaseProvider } from "../database/database";
+import { DatabaseProvider } from '../database/database';
 import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcrypt';
 import * as config from '../../config.json';
 import { User } from '../models';
+import { AuthData } from '../types';
 
 export class AuthService {
-  public async authenticate(userData: any): Promise<any> {
+  public async authenticate(userData: AuthData): Promise<any> {
     console.log(`authenticating ${userData.login}`);
     const connection = await DatabaseProvider.getConnection();
-    
+
     try {
       const pwd = await bcrypt.hash(userData.password, 12);
       const user = new User();
       user.login = userData.login;
-      user.password = pwd
-      const exists = await connection.mongoManager.findOne(User, {login: userData.login});
+      user.password = pwd;
+      const exists = await connection.mongoManager.findOne(User, {
+        login: userData.login
+      });
+
       if (exists && exists.id) {
         return {
           status: 400,
@@ -22,48 +26,51 @@ export class AuthService {
             success: false,
             error: 'such user already exists'
           }
-        }
+        };
       }
-      const result = await connection.mongoManager.save(user);
-      const token = jwt.sign({...result}, config.jwt.secret, {
-        expiresIn: '45m'
-      });
 
-      const {iat, exp} = jwt.decode(token);
-      return { status: 200,  data: { iat, exp, token } }
+      const result = await connection.mongoManager.save(user);
+
+      return { status: 200, data: this.signToken(result) };
     } catch (error) {
       return { status: 400, data: { error } };
     }
   }
 
-  public async login(userData: any): Promise<any> {
+  public async login(userData: AuthData): Promise<any> {
     console.log(`logging in as ${userData.login}`);
 
     const connection = await DatabaseProvider.getConnection();
 
     try {
-      const user = await connection.mongoManager.findOne(User, {login: userData.login});
+      const user = await connection.mongoManager.findOne(User, {
+        login: userData.login
+      });
       const match = await bcrypt.compare(userData.password, user.password);
+
       if (!match) {
         return {
           status: 403,
           data: {
             success: false,
-            error: 'Passwords don\'t match'
+            error: "Passwords don't match"
           }
-        }
+        };
       }
 
-      const token = jwt.sign({...user}, config.jwt.secret, {
-        expiresIn: '45m'
-      });
-
-      const {iat, exp} = jwt.decode(token);
-      return { status: 200, data: { iat, exp, token } }
-
+      return { status: 200, data: this.signToken(user) };
     } catch (error) {
       return { status: 400, data: { success: false, error } };
     }
+  }
+
+  private signToken(user: User): Object {
+    const token = jwt.sign({ ...user }, config.jwt.secret, {
+      expiresIn: '45m'
+    });
+
+    const { iat, exp } = jwt.decode(token);
+    return { iat, exp, token };
   }
 }
 
